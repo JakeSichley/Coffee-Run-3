9775ed545a3da15c3e9584b3ff3ac0f266d01219
Jake Sichley <39101429+JakeSichley@users.noreply.github.com>
2020-10-22 23:17:58 -0700

    Update RemoteDatastore to use Firebase

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,7 @@
     'use strict';
     
     var FORM_SELECTOR = '[data-coffee-order="form"]';
-    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';     // CHOOSE ONLY ONE...
-    var SERVER_URL = 'http://localhost:3000/coffeeorders';          // if running locally
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
     var App = window.App;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
@@ -11,7 +10,7 @@
     var FormHandler = App.FormHandler;
     var Validation = App.Validation;
     var CheckList = App.CheckList;
-    var remoteDS = new RemoteDataStore(SERVER_URL);
+    var remoteDS = new RemoteDataStore();
     var truck = new Truck('ncc-1701', remoteDS);
     window.truck = truck;
     var checkList = new CheckList(CHECKLIST_SELECTOR);
--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -4,35 +4,61 @@
     var App = window.App || {};
     var $ = window.jQuery;
 
+    var firebaseConfig = {
+        apiKey: "HIDDEN",
+        authDomain: "coffee-run-c2aa8.firebaseapp.com",
+        databaseURL: "https://coffee-run-c2aa8.firebaseio.com",
+        projectId: "coffee-run-c2aa8",
+        storageBucket: "coffee-run-c2aa8.appspot.com",
+        messagingSenderId: "813099354338",
+        appId: "1:813099354338:web:95abb955e2e6ed1a9a36bb",
+        measurementId: "G-8RDVM56MGH"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
     class RemoteDataStore {
-        constructor(url) {
-            console.log('running the DataStore function');
-            if (!url) { throw new Error('No remote URL supplied.'); }
-
-            this.serverURL = url;
+        constructor() {
+            console.log('running the Firestore function');
+            this.db = firebase.firestore();   
         }
 
-        ajaxposthelper(type, url, val) {
-            $.ajax({ type: type, url: url, contentType: 'application/json',
-                data: JSON.stringify(val), success: function(response) { 
-                    console.log('function returned: ' + JSON.stringify(response));
-                }
+        add(key, val) {
+            const ordersRef = this.db.collection('Orders');
+            ordersRef.doc(key).set(val).then((ref) => {
+                console.log("Added doc with ID: ", key);
             });
         }
-        
-        ajaxhelper(type, url, cb) {
-            $.ajax({ type: type, url: url, contentType: 'application/json',
-                success: function(response) { 
-                    console.log('function returned: ' + JSON.stringify(response));
-                    if (cb !== undefined) { cb(response); }
-                }
-            });
+
+        remove(key) {
+            const ordersRef = this.db.collection('Orders');
+            ordersRef.doc(key).delete().then(() => console.log("Order for ", key, " deleted"))
+            .catch((error) => console.error("Error deleting document", error));
         }
-        add(key, val) { this.ajaxposthelper('POST',   this.serverURL,             val); }
-        get(key, cb)  { this.ajaxhelper    ('GET',    this.serverURL + '/' + key, cb); }
-        getAll(cb)    { this.ajaxhelper    ('GET',    this.serverURL,             cb); }
-        remove(key)   { this.ajaxhelper    ('DELETE', this.serverURL + '/' + key); } 
+
+        get(key) {
+            console.log('Retrieving order with key: ', key);
+            const ordersRef = this.db.collection('Orders');
+            const specificOrder = ordersRef.doc(key);
+
+            specificOrder.get().then((doc) => {
+                if (!doc.exists) {
+                    return;
+                } else {
+                    return doc.data();
+                }
+            })
+        }
+
+        async getAll() {
+            console.log('Retrieving all orders');
+            const ordersRef = this.db.collection('Orders');
+            const snapshot = await ordersRef.get();
+            return snapshot.docs.map(doc => doc.data());
+        }
     }
+
     App.RemoteDataStore = RemoteDataStore;
     window.App = App;
     
